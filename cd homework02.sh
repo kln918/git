c94feb5d906617f64141a71163c552a8421ccf00
Kelcey Nicholas <kelceynicholas@icloud.com>
2023-10-16 20:01:07 -0700

    Update cd homework02.sh
    added name change code

--- cd homework02.sh
+++ cd homework02.sh
@@ -4,3 +4,29 @@ cd homework2
 
 for i in {1..25} ; do echo text_$i > text_file_$i.txt ; done
 
+kelceynicholas@Kelceys-MacBook-Air homework2 % mv text_file_1.txt file504.txt
+kelceynicholas@Kelceys-MacBook-Air homework2 % mv text_file_2.txt file569.txt
+kelceynicholas@Kelceys-MacBook-Air homework2 % mv text_file_3.txt file231.txt
+kelceynicholas@Kelceys-MacBook-Air homework2 % mv text_file_4.txt file717.txt
+kelceynicholas@Kelceys-MacBook-Air homework2 % mv text_file_5.txt file986.txt
+kelceynicholas@Kelceys-MacBook-Air homework2 % mv text_file_6.txt file528.txt
+kelceynicholas@Kelceys-MacBook-Air homework2 % mv text_file_7.txt file759.txt
+kelceynicholas@Kelceys-MacBook-Air homework2 % mv text_file_8.txt file654.txt
+kelceynicholas@Kelceys-MacBook-Air homework2 % mv text_file_9.txt file751.txt
+kelceynicholas@Kelceys-MacBook-Air homework2 % mv text_file_10.txt file262.txt
+kelceynicholas@Kelceys-MacBook-Air homework2 % mv text_file_11.txt file889.txt
+kelceynicholas@Kelceys-MacBook-Air homework2 % mv text_file_12.txt file281.txt
+kelceynicholas@Kelceys-MacBook-Air homework2 % mv text_file_13.txt file186.txt
+kelceynicholas@Kelceys-MacBook-Air homework2 % mv text_file_14.txt file108.txt
+kelceynicholas@Kelceys-MacBook-Air homework2 % mv text_file_15.txt file388.txt
+kelceynicholas@Kelceys-MacBook-Air homework2 % mv text_file_16.txt file374.txt
+kelceynicholas@Kelceys-MacBook-Air homework2 % mv text_file_17.txt file148.txt
+kelceynicholas@Kelceys-MacBook-Air homework2 % mv text_file_18.txt file716.txt
+kelceynicholas@Kelceys-MacBook-Air homework2 % mv text_file_19.txt file637.txt
+kelceynicholas@Kelceys-MacBook-Air homework2 % mv text_file_20.txt file550.txt
+kelceynicholas@Kelceys-MacBook-Air homework2 % mv text_file_21.txt file382.txt
+kelceynicholas@Kelceys-MacBook-Air homework2 % mv text_file_22.txt file797.txt
+kelceynicholas@Kelceys-MacBook-Air homework2 % mv text_file_23.txt file477.txt
+kelceynicholas@Kelceys-MacBook-Air homework2 % mv text_file_24.txt file202.txt 
+kelceynicholas@Kelceys-MacBook-Air homework2 % mv text_file_25.txt file699.txt
+
